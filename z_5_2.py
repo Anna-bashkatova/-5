dff21c8583c69885d2723d012fc4219d0a7e46c5
Анна Башкатова <BashkatovaAD320@mgpu.ru>
2020-12-02 22:00:00 +0300

    5

--- z_5_2.py
+++ z_5_2.py
@@ -1,4 +1,20 @@
-from geometry import *
-a = planimetry.Rectangle(3, 4)
-b = stereometry.Ball(5)
-print(a.square())
+from zadanie_5_2 import *
+
+print("Размер помещения:")
+l = float(input("Длина - "))
+w = float(input("Ширина - "))
+h = float(input("Высота - "))
+r1 = module1.Room(l, w, h)
+
+flag = input("Есть неоклеиваемая поверхность? (1 - да, 2 - нет)")
+while flag == '1':
+  w = float(input("Ширина - "))
+  h = float(input("Высота - "))
+  r1.addWD(w, h)
+  flag = input("Добавить еще? (1 - да, 2 - нет) ")
+
+print("Размер рулона:")
+l = float(input("Длина - "))
+w = float(input("Ширина - "))
+print("Площадь оклейки", r1.workSurface())
+print("Количество рулонов", r1.wallpapers(l, w))
